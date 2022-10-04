48d37c09054d3c959ce8123ef4e0702be6bb9998
DungAT <dungat98@gmail.com>
2022-10-04 13:54:09 +0700

    Change ITransaction to public access modifier

--- SqlBulkTools/ITransaction.cs
+++ SqlBulkTools/ITransaction.cs
@@ -3,11 +3,27 @@ using Microsoft.Data.SqlClient;
 
 namespace SqlBulkTools
 {
-    internal interface ITransaction
+    /// <summary>
+    /// This is infrastructure of transaction
+    /// </summary>
+    public interface ITransaction
     {
+        /// <summary>
+        /// Commit transaction when executing bulk operations
+        /// </summary>
+        /// <param name="connectionName"></param>
+        /// <param name="credentials"></param>
+        /// <param name="connection"></param>
         void CommitTransaction(string connectionName = null, SqlCredential credentials = null,
             SqlConnection connection = null);
 
+        /// <summary>
+        /// Async method when commit transaction when executing bulk operations
+        /// </summary>
+        /// <param name="connectionName"></param>
+        /// <param name="credentials"></param>
+        /// <param name="connection"></param>
+        /// <returns></returns>
         Task CommitTransactionAsync(string connectionName = null, SqlCredential credentials = null,
             SqlConnection connection = null);
     }
